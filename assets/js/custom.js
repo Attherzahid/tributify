c088c0a80c6d9e158ff2ae86a9623e0d2b8dadd6
Attherzahid <attherzahid786@gmail.com>
2024-11-21 05:54:11 +0500

    final update.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -126,5 +126,46 @@ jQuery(document).ready(function () {
     dots: false
   });
 
+  $(".nav-link").on("click", function (e) {
+    e.preventDefault();
+
+    // Remove active class from all tabs and contents
+    $(".nav-link").removeClass("active");
+    $(".tab-pane").removeClass("active");
+
+    // Add active class to the clicked tab and corresponding content
+    $(this).addClass("active");
+    const target = $(this).attr("href");
+    $(target).addClass("active");
+  });
+
+  // modal satrt
+  $('[data-target]').click(function () {
+    const targetModal = $(this).data('target'); // Get the target modal from data-target attribute
+    $(targetModal).addClass('fade show'); // Show the targeted modal
+  });
+
+  // Close modal when the 'x' is clicked
+  $('.close-modal').click(function () {
+    $(this).closest('.modal').removeClass('fade show'); // Close the closest modal
+  });
+
+
+  // Close modal if clicked outside the modal content
+  $(window).click(function (event) {
+    if ($(event.target).is('.modal')) {
+      $(event.target).removeClass('fade show'); // Close the modal if the overlay is clicked
+    }
+  });
+
+  $('#passwordToggler').on('click', function () {
+    let passInput = $('#loginPassword');
+    if (passInput.attr('type') == 'password') {
+      passInput.attr('type', 'text');
+    } else {
+      passInput.attr('type', 'password');
+    }
+
+  });
 
 });
